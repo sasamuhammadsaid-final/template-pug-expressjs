1e2e731a09a4863d05cef6ceeb4c0b90e6122eab
Sasa Muhammad Said <sasamuhammadsaid.final@gmail.com>
2025-06-20 02:38:57 +0700

    Finishing the pug template

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,9 @@
-const path = require("path");
 const express = require("express");
 const router = express.Router();
-const rootDir = require("../utils/path");
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 router.post("/add-product", (req, res, next) => {
